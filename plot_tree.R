84c74db045c23c89e651ecf6c2cc2d6f7a84a7df
PeterMulhair <bspm@leeds.ac.uk>
2021-02-11 11:28:36 +0000

    Rscript for pdf output

---
+++ plot_tree.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+library(ggtree)
+library(getopt)
+
+#Get call input
+spec <- matrix(c('phylo', 't', 1, "character", 'output', 'o', 1, "character"), byrow=TRUE, ncol=4)
+
+opt <- getopt(spec)
+
+#Read rooted tree file
+tree <- read.newick(opt$phylo)
+
+#Create tree object
+if(tree$Nnode<80) {
+p <- ggtree(tree) + geom_tiplab(offset = 0.00001)
+} else {p <- ggtree(tree, layout="circular") + geom_tiplab2()}
+
+
+#Write tree to pdf
+if(tree$Nnode<40){
+pdf(opt$output, "_tree.pdf", height = 20, width = 50)
+} else { if(tree$Nnode<60) {
+pdf(opt$output, "_tree.pdf", height = 50, width = 50)
+} else {pdf(opt$output, "_tree.pdf", height = 150, width = 150)
+}
+}
+print(p)
+dev.off()
